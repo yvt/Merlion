b09901b7e11d6e8249d83f0688f28e751e0daf8f
yvt <i@yvt.jp>
2014-10-02 19:44:17 +0900

    Removed use of initialized global variable

--- MerlionServerCore/Exceptions.cpp
+++ MerlionServerCore/Exceptions.cpp
@@ -4,19 +4,30 @@
 
 namespace mcore
 {
-	// For unknown reason, boost::thread_specific_ptr<std::string>::
+	// 1. For unknown reason, boost::thread_specific_ptr<std::string>::
 	// ~boost::thread_specific_ptr() crashes on exit.
 	// So leave it leaked...
-    static boost::thread_specific_ptr<std::string> *lastErrorMessage =
-	new boost::thread_specific_ptr<std::string>();
+	// 2. Initialized static (non-local) global variable is
+	// likely to crash the shared library
+	namespace
+	{
+		boost::thread_specific_ptr<std::string>& lastErrorMessage()
+		{
+			static boost::thread_specific_ptr<std::string> *lastErrorMessage = nullptr;
+			if (lastErrorMessage == nullptr) {
+				lastErrorMessage = new boost::thread_specific_ptr<std::string>();
+			}
+			return *lastErrorMessage;
+		}
+	}
 
     void setLastErrorMessage(const std::string& s)
     {
-        lastErrorMessage->reset(new std::string(s));
+        lastErrorMessage().reset(new std::string(s));
     }
     const char *getLastErrorMessage()
     {
-        std::string *msg = lastErrorMessage->get();
+        std::string *msg = lastErrorMessage().get();
         if (msg == nullptr) {
             return "No error occured yet.";
         } else {
