d8de12f1cfe7aef3f8d11bcbf35eb4768424ed7b
yvt <i@yvt.jp>
2014-10-12 23:50:03 +0900

    MerlionServerTest now works

--- MerlionServerTest/Test.cs
+++ MerlionServerTest/Test.cs
@@ -10,20 +10,12 @@ namespace Merlion.Server.Test
 		[Test ()]
 		public void Create ()
 		{
-			using (var baseDir = new TemporaryDirectory ()) {
-				var settings = new NameValueCollection ();
-				settings ["EndpointAddress"] = "0.0.0.0:16070";
-				settings ["MasterServerAddress"] = "0.0.0.0:5000";
-				settings ["BaseDirectory"] = baseDir.Path;
-				AppConfiguration.AppSettings = settings;
+			TestUtils.MakeTestServerEnvironment (() => {
+				TestUtils.SetupSelfSignedServerCertificate();
 
-				var p = new NativeServerInterop.MasterParameters ();
-				p.ClientEndpoint = "0.0.0.0:16070";
-				p.NodeEndpoint = "0.0.0.0:5000";
-
-				using (var master = new NativeMasterServer (p)) {
+				using (var master = new NativeMasterServer ()) {
 				}
-			}
+			});
 		}
 	}
 }
---
+++ MerlionServerTest/TestUtils.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Specialized;
+
+namespace Merlion.Server.Test
+{
+	static class TestUtils
+	{
+		public static void MakeTestServerEnvironment(Action a)
+		{
+			using (var baseDir = new TemporaryDirectory ()) {
+				var settings = new NameValueCollection ();
+				settings ["EndpointAddress"] = "0.0.0.0:16070";
+				settings ["MasterServerAddress"] = "0.0.0.0:5000";
+				settings ["BaseDirectory"] = baseDir.Path;
+				
+				AppConfiguration.AppSettings = settings;
+
+				a ();
+			}
+		}
+
+		public static void SetupSelfSignedServerCertificate()
+		{
+			var settings = AppConfiguration.AppSettings;
+			if (settings["MasterServerCertificate"] != null &&
+				settings["MasterServerPrivateKey"] != null) {
+				return;
+			}
+
+			settings ["MasterServerCertificate"] = "cert.crt";
+			settings ["MasterServerPrivateKey"] = "cert.key";
+
+			using (var s = System.Reflection.Assembly.GetExecutingAssembly().GetManifestResourceStream
+				("Merlion.Server.Test.Resources.TestCertificate.crt"))
+			using (var s2 = System.IO.File.OpenWrite(AppConfiguration.MasterServerCertificate)) {
+				s.CopyTo (s2);
+			}
+			using (var s = System.Reflection.Assembly.GetExecutingAssembly().GetManifestResourceStream
+				("Merlion.Server.Test.Resources.TestCertificate.key"))
+			using (var s2 = System.IO.File.OpenWrite(AppConfiguration.MasterServerPrivateKey)) {
+				s.CopyTo (s2);
+			}
+		}
+	}
+}
+
